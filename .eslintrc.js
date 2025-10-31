3b960c805262146be1daa8199c9e2254287540af
Khaai <khaaimach@gmail.com>
2025-10-31 21:53:13 +0700

    Cau hinh cho du an

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    node: true, [cite: 174]
+    commonjs: true, [cite: 175]
+    es2021: true, [cite: 176]
+  },
+  extends: ['eslint:recommended', 'prettier'], [cite: 178]
+};
